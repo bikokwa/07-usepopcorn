d26ef7b10d70727bc28ca375d5f762b1683a4342
bikokwa <34458781+bikokwa@users.noreply.github.com>
2024-04-16 18:34:29 +0300

    use an async function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -191,14 +191,23 @@ function Main({ children }) {
 }
 
 const KEY = "2c7b6b17";
+const query = "interstellar";
 
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
 
-  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`).then((res) =>
-    res.json().then((data) => console.log(data.Search))
-  );
+  useEffect(function () {
+    async function fetchMovies() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+    }
+
+    fetchMovies();
+  }, []);
 
   return (
     <>
